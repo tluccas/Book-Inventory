d3c34329f47fb840dcce568065110f6887a4db4f
Lucas Alves <lucas1901alves@gmail.com>
2025-12-14 21:52:49 -0300

    feat(model): add BookOnLoan model

---
+++ src/main/java/com/github/tluccas/book_inventory/database/model/Emprestimo/BookOnLoan.java
@@ -0,0 +1,11 @@
+package com.github.tluccas.book_inventory.database.model.Emprestimo;
+
+import lombok.Data;
+
+@Data
+public class BookOnLoan {
+    
+    private String bookId;
+    private String loanId;
+    
+}
