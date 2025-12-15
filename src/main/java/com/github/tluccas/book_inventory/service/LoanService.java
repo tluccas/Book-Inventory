72d1194cbc9f76831538ae68a50627e0af71822a
Lucas Alves <lucas1901alves@gmail.com>
2025-12-14 21:53:02 -0300

    feat(service): add LoanService

---
+++ src/main/java/com/github/tluccas/book_inventory/service/LoanService.java
@@ -0,0 +1,76 @@
+package com.github.tluccas.book_inventory.service;
+
+import org.springframework.stereotype.Service;
+
+import com.github.tluccas.book_inventory.database.model.Emprestimo.Loan;
+import com.github.tluccas.book_inventory.database.model.Emprestimo.LoanStatus;
+import com.github.tluccas.book_inventory.dto.CreateLoanReq;
+import com.github.tluccas.book_inventory.exceptions.NotFoundException;
+
+import net.ravendb.client.documents.DocumentStore;
+import net.ravendb.client.documents.session.IDocumentSession;
+import java.util.List;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import com.github.tluccas.book_inventory.database.model.Emprestimo.BookOnLoan;
+import com.github.tluccas.book_inventory.database.model.User;
+@Service
+public class LoanService {
+    
+    private final BookService bookService;
+    private final UserService userService;
+    private final DocumentStore store;
+
+    public LoanService(DocumentStore store, BookService bookService, UserService userService){
+        this.store = store;
+        this.bookService = bookService;
+        this.userService = userService;
+    }
+
+    public Loan create(CreateLoanReq req){
+        try (IDocumentSession session = store.openSession()) {
+
+            User user = userService.findById(req.userId());
+            if (user == null) {
+                throw new NotFoundException("Usuário não encontrado: " + req.userId());
+            }
+
+            List<BookOnLoan> booksToLoan = new ArrayList<>(); // Lista auxiliar para armazenar os livros válidos
+
+            for (BookOnLoan bookOnLoan : req.books()){   
+                String bookId = bookOnLoan.getBookId();
+
+                if (bookService.findById(bookId) == null) {
+                    throw new NotFoundException("Livro não encontrado: " + bookId);
+                }
+
+                booksToLoan.add(bookOnLoan);
+            }
+            
+            Loan createdLoan = new Loan();
+
+            createdLoan.setUserId(user.getId());
+            createdLoan.setBooks(booksToLoan);
+            createdLoan.setLoanDate(LocalDate.now());
+            createdLoan.setReturnDate(req.returnDate());
+            createdLoan.setStatus(LoanStatus.DEVOLUCAO_PENDENTE);
+            session.store(createdLoan);
+            session.saveChanges();
+            return createdLoan;
+        }
+    }
+
+    public List<Loan> findAll(){
+        try (IDocumentSession session = store.openSession()) {
+            return session.query(Loan.class).toList(); 
+        }
+    }
+
+    public Loan findById(String id){
+        try (IDocumentSession session = store.openSession()) {
+            return session.load(Loan.class, id);
+        }
+    }
+
+    
+}
