2d2aaac080ea8c0be92b082db0db1e34a90b3192
Lucas Alves <lucas1901alves@gmail.com>
2025-12-14 21:52:59 -0300

    feat(dto): add CreateLoanReq DTO

---
+++ src/main/java/com/github/tluccas/book_inventory/dto/CreateLoanReq.java
@@ -0,0 +1,8 @@
+package com.github.tluccas.book_inventory.dto;
+import com.github.tluccas.book_inventory.database.model.Emprestimo.BookOnLoan;
+import java.util.List;
+import java.time.LocalDate;
+public record CreateLoanReq( String userId, 
+                             List<BookOnLoan> books,
+                             LocalDate returnDate
+                              ) {}
