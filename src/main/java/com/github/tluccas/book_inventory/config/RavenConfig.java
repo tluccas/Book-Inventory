3c2f14ded809af8911d4e2ad18df2c0f45bb61d8
Lucas Alves <lucas1901alves@gmail.com>
2025-12-14 21:52:44 -0300

    feat(config): add RavenDB configuration

---
+++ src/main/java/com/github/tluccas/book_inventory/config/RavenConfig.java
@@ -0,0 +1,19 @@
+package com.github.tluccas.book_inventory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import net.ravendb.client.documents.DocumentStore;
+
+@Configuration
+public class RavenConfig {
+    
+    @Bean
+    public DocumentStore documentStore(){
+        DocumentStore store = new DocumentStore();
+            store.setUrls(new String[]{"http://localhost:8080"}); // Server URL
+            store.setDatabase("book_inventory_db"); // Banco de dados padrão
+            store.initialize();
+            return store;
+    }
+}
